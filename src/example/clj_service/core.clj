9d1500160202207a692a63afe22fde429cec0d65
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-29 23:08:39 +0300

    core: add system lifecycle on main entrypoint

--- src/example/clj_service/core.clj
+++ src/example/clj_service/core.clj
@@ -1,7 +1,19 @@
 (ns example.clj-service.core
   (:require
-   [clojure.tools.logging :as log])
+   [clojure.tools.logging :as log]
+   [example.clj-service.system :as system])
   (:gen-class))
 
+(defn add-shutdown-hook [f]
+  (let [shutdown-fn
+        (fn []
+          (f)
+          (shutdown-agents))]
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook
+         (Thread. shutdown-fn)))))
+
 (defn -main [& args]
-  (log/info "Hello there!"))
+  (log/info "Hello there!")
+  (let [system (system/start)]
+    (add-shutdown-hook #(system/stop system))))
