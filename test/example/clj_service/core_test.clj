90e37b8d915fc1a411142a6c5306c9904b52bc6f
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-29 19:47:25 +0300

    project: beautify generated sources

--- test/example/clj_service/core_test.clj
+++ test/example/clj_service/core_test.clj
@@ -1,6 +1,7 @@
 (ns example.clj-service.core-test
-  (:require [clojure.test :refer :all]
-            [example.clj-service.core :refer :all]))
+  (:require
+   [clojure.test :refer :all]
+   [example.clj-service.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
