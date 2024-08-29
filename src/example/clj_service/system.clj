c458ff44d87688b66017560a811a5f4e5ea272be
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-29 22:37:21 +0300

    system: add skeleton

---
+++ src/example/clj_service/system.clj
@@ -0,0 +1,28 @@
+(ns example.clj-service.system
+  (:require
+   [clojure.tools.logging :as log]
+   [integrant.core :as integrant]
+   [example.clj-service.server :as server]))
+
+(defn system-map []
+  {::server {}})
+
+(defn start []
+  (log/info "Starting system")
+  (-> (system-map)
+      (integrant/init)))
+
+(defn stop [system]
+  (log/info "Stopping system")
+  (-> system
+      (integrant/halt!)))
+
+(defmethod integrant/init-key ::server [_ opts]
+  (server/start))
+
+(defmethod integrant/halt-key! ::server [_ server]
+  (server/stop server))
+
+(comment
+  (def system (start))
+  (stop system))
