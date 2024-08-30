9b219788412bea28386b8bafaf1cf2c39f756a71
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-30 12:03:26 +0300

    dev: add integrant repl setup

--- dev/user.clj
+++ dev/user.clj
@@ -1,1 +1,11 @@
-(ns user)
+(ns user
+  (:require
+   [integrant.repl :refer [clear go halt prep init reset reset-all]]
+   [example.clj-service.config :as config]
+   [example.clj-service.system :as system]))
+
+(integrant.repl/set-prep!
+ (fn []
+   (-> (config/config-map)
+       (system/system-map)
+       (update-vals #(assoc % :dev? true)))))
